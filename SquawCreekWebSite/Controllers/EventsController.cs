1093788eaff1630903418d169c670b4d774d7f14
millydev <lovelyperuvian1@gmail.com>
2017-08-16 22:33:26 -0400

    changes to framework and remove unnecesary libraries.

--- SquawCreekWebSite/Controllers/EventsController.cs
+++ SquawCreekWebSite/Controllers/EventsController.cs
@@ -2,12 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using System.Web.Http.Description;
-using System.Web.Mvc;
-using System.Web.Script.Serialization;
 using SquawCreekWebSite.DB;
 using SquawCreekWebSite.Models;
 
